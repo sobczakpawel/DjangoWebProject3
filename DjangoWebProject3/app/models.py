24efcad02abe9a3a2edab83904125d7cab472b6c
pawelWork <sobczak.pawel91@gmail.com>
2017-07-18 16:41:02 +0200

    01

--- DjangoWebProject3/app/models.py
+++ DjangoWebProject3/app/models.py
@@ -5,3 +5,12 @@ Definition of models.
 from django.db import models
 
 # Create your models here.
+class Categories(models.Model):
+    name = models.CharField(max_length = 30)
+    description = models.CharField(max_length = 60)
+
+class Products(models.Model):
+    name = models.CharField(max_length = 20)
+    index = models.CharField(max_length = 20)
+    price = models.DecimalField(max_digits = 6, decimal_places = 2)
+    categorie = models.ForeignKey(Categories)
