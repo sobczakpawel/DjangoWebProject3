ba6f5898795fe55dcc20029c87adbde0e39664eb
pawelWork <sobczak.pawel91@gmail.com>
2017-07-18 14:45:53 +0200

    init

---
+++ DjangoWebProject3/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\dragan\documents\visual studio 2017\Projects\DjangoWebProject3\DjangoWebProject3\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
